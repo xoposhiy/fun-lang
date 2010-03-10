759813271149eb191123b6f3b9cb1e7eda7d92ce
xoposhiy@mail.ru <xoposhiy@mail.ru@dce0a5f0-d1f0-11dd-a412-a1792178251d>
2010-03-10 19:26:17 +0000

    Инфраструктурка для упрощения добавления новых бинарных функций.

--- oop.term2.fun/src/ru/usu/cs/fun/lang/Add.java
+++ oop.term2.fun/src/ru/usu/cs/fun/lang/Add.java
@@ -3,40 +3,18 @@ package ru.usu.cs.fun.lang;
 import ru.usu.cs.fun.back.EvalObserver;
 import ru.usu.cs.fun.back.Scope;
 import ru.usu.cs.fun.back.Term;
-import ru.usu.cs.fun.back.TermsSubstitutor;
 
-public class Add extends Term {
+public class Add extends BinaryOperation {
 
-	@Override
-	public Term apply(Term arg, Scope scope, EvalObserver observer) {
-		return new AddCont(arg);
-	}
-
-	private static class AddCont extends Term {
-
-		private final Term arg1;
-
-		public AddCont(Term arg1) {
-			this.arg1 = arg1;
-		}
-
-		@Override
-		public Term apply(Term arg2, Scope scope, EvalObserver observer) {
-			int v1 = ((Int) arg1.eval(scope, observer)).value;
-			int v2 = ((Int) arg2.eval(scope, observer)).value;
-			return new Int(v1 + v2);
-		}
-
-		@Override
-		public String toString(TermsSubstitutor subst) {
-			return "+" + subst.substitute(arg1);
-		}
-
+	public Add() {
+		super("+");
 	}
 
 	@Override
-	public String toString(TermsSubstitutor subst) {
-		return "+";
+	protected Term calculate(Term arg1, Term arg2, Scope scope, EvalObserver observer) {
+		int v1 = ((Int) arg1).value;
+		int v2 = ((Int) arg2.eval(scope, observer)).value;
+		return new Int(v1 + v2);
 	}
 
 }
--- oop.term2.fun/src/ru/usu/cs/fun/lang/Eq.java
+++ oop.term2.fun/src/ru/usu/cs/fun/lang/Eq.java
@@ -3,43 +3,16 @@ package ru.usu.cs.fun.lang;
 import ru.usu.cs.fun.back.EvalObserver;
 import ru.usu.cs.fun.back.Scope;
 import ru.usu.cs.fun.back.Term;
-import ru.usu.cs.fun.back.TermsSubstitutor;
 
-public class Eq extends Term {
+public class Eq extends BinaryOperation {
 
-	@Override
-	public Term apply(Term arg, Scope scope, EvalObserver observer) {
-		return new EqCont(arg);
+	public Eq() {
+		super("=");
 	}
 
 	@Override
-	public String toString(TermsSubstitutor subst) {
-		return "=";
-	}
-
-	// TODO Сделать так, чтобы подобные классы не приходилось плодить вручную.
-	private static class EqCont extends Term {
-
-		private Term arg1;
-		private boolean evaluated = false;
-
-		public EqCont(Term arg1) {
-			this.arg1 = arg1;
-		}
-
-		@Override
-		public String toString(TermsSubstitutor subst) {
-			return "=" + subst.substitute(arg1).toString();
-		}
-
-		@Override
-		public Term apply(Term arg, Scope scope, EvalObserver observer) {
-			if (!evaluated) {
-				arg1 = arg1.eval(scope, observer);
-				evaluated = true;
-			}
-			return Bool.from(arg1.equals(arg.eval(scope, observer)));
-		}
+	protected Term calculate(Term arg1, Term arg2, Scope scope, EvalObserver observer) {
+		return Bool.from(arg1.equals(arg2.eval(scope, observer)));
 	}
 
 }
